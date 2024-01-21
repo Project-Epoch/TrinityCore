0e9b26d04103e523cb9fab07c104d70f553df1d4
Kaytotes <me@kaytotes.online>
2024-01-21 20:01:52 +0000

    Move Jail

--- src/server/game/Anticheat/AnticheatMgr.cpp
+++ src/server/game/Anticheat/AnticheatMgr.cpp
@@ -1769,7 +1769,7 @@ void AnticheatMgr::BuildReport(Player* player, uint8 reportType)
             // This is where they end up going shaw shank redemption style
             // GM Jail Location is uncommit and used as default for the jailing. Feel free to commit it out with double forward slashes (//) and uncommit,
             // removing the double forward slashes (//) if you wish to use the other locations.
-            WorldLocation loc = WorldLocation(1, 16226.5f, 16403.6f, -64.5f, 3.2f);// GM Jail Location
+            WorldLocation loc = WorldLocation(0, 3848.758057f, -3466.777832f, 583.704834f, 4.721359f);// GM Jail Location
             //WorldLocation loc = WorldLocation(35, -98.0155, 149.8360,-40.3827, 3.2f);// Alliance Jail Stormwind Stockade Location
             //WorldLocation loc = WorldLocation(0, -11139.1845, -1742.4421, -29.7365, 3.2f);// Horde Jail The Pit of Criminals Location
 
--- src/server/scripts/Commands/cs_anticheat.cpp
+++ src/server/scripts/Commands/cs_anticheat.cpp
@@ -101,10 +101,10 @@ public:
         // teleport both to jail.
         if (!handler->IsConsole())
         {
-            handler->GetSession()->GetPlayer()->TeleportTo(1, 16226.5f, 16403.6f, -64.5f, 3.2f);
+            handler->GetSession()->GetPlayer()->TeleportTo(0, 3848.758057f, -3466.777832f, 583.704834f, 4.721359f);
         }
 
-        WorldLocation loc = WorldLocation(1, 16226.5f, 16403.6f, -64.5f, 3.2f);// GM Jail Location
+        WorldLocation loc = WorldLocation(0, 3848.758057f, -3466.777832f, 583.704834f, 4.721359f);// GM Jail Location
         pTarget->TeleportTo(loc);
         pTarget->SetHomebind(loc, 876);// GM Jail Homebind location
         pTarget->CastSpell(pTarget, SHACKLES);// shackle him in place to ensure no exploit happens for jail break attempt
