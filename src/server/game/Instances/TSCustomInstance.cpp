111877932d4b1bcd310ed1433df7e9a912c2a5b6
Kaytotes <me@kaytotes.online>
2024-05-29 22:13:28 +0100

    Read and Write

--- src/server/game/Instances/TSCustomInstance.cpp
+++ src/server/game/Instances/TSCustomInstance.cpp
@@ -1,10 +1,60 @@
 #include "TSCustomInstance.h"
 
-/**
- * Real loading in respective function
-*/
 TSCustomInstance::TSCustomInstance(InstanceMap* map) : InstanceScript(map) {
+    // Real loading in respective functions.
+    SetHeaders(DataHeader);
     SetBossNumber(0);
     LoadBossBoundaries({});
     LoadDoorData(nullptr);
+}
+
+void TSCustomInstance::WriteSaveDataMore(std::ostringstream& data)
+{
+    TSArray<uint32> _data;
+
+    FIRE_ID(
+          instance->GetEntry()->ID
+        , Instance,OnWriteSaveDataMore
+        , TSInstance(instance, this)
+        , TSMutable<TSArray<uint32>, TSArray<uint32>>(&_data)
+    );
+
+    _data.forEach([&data](uint32 v, size_t i, TSArray<uint32>& arr) {
+        if (i == 0)
+            data << static_cast<uint32>(v);
+        else
+            data << ' ' << static_cast<uint32>(v);
+    });
+}
+
+void TSCustomInstance::ReadSaveDataMore(std::istringstream& data)
+{
+    uint8 length = 0;
+
+    FIRE_ID(
+          instance->GetEntry()->ID
+        , Instance,OnBeforeReadSaveDataMore
+        , TSInstance(instance, this)
+        , TSMutable<uint8, uint8>(&length)
+    );
+
+    if (length == 0)
+        return;
+
+    TSArray<uint32> _data;
+
+    for (uint8 i = 0; i < length; i++) {
+        uint32 value;
+
+        data >> value;
+
+        _data.insert(i, value);
+    }
+
+    FIRE_ID(
+          instance->GetEntry()->ID
+        , Instance,OnReadSaveDataMore
+        , TSInstance(instance, this)
+        , _data
+    );
 }
--- src/server/game/Instances/TSCustomInstance.h
+++ src/server/game/Instances/TSCustomInstance.h
@@ -1,11 +1,18 @@
 #ifndef TSCUSTOMINSTANCE_H
 #define TSCUSTOMINSTANCE_H
 
+#include "TSMutable.h"
+#include "Map.h"
 #include "InstanceScript.h"
+
+#define DataHeader "TS"
 
 class TC_GAME_API TSCustomInstance : public InstanceScript {
 public:
     TSCustomInstance(InstanceMap* map);
+
+    void WriteSaveDataMore(std::ostringstream& /*data*/) override;
+    void ReadSaveDataMore(std::istringstream& /*data*/) override;
 };
 
 #endif
