f0c03f1d4a91a921493696ebf2a3c6504e18f55b
hater <hater.rip@proton.me>
2024-06-23 16:17:08 -0400

    update sql remove direct tables

--- sql/custom/characters/20230101_01.sql
+++ sql/custom/characters/20230101_01.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXiSTS `characters`.`character_accountwide_reputation`;
-CREATE TABLE `characters`.`character_accountwide_reputation` (
+DROP TABLE IF EXiSTS `character_accountwide_reputation`;
+CREATE TABLE `character_accountwide_reputation` (
   `accountId` INT UNSIGNED NOT NULL,
   `factionGroup` INT UNSIGNED NOT NULL,
   `factionId` INT UNSIGNED NOT NULL,
   `rep` INT UNSIGNED NULL,
   PRIMARY KEY (`accountId`, `factionGroup`, `factionId`));
 
-DROP TABLE IF EXiSTS `characters`.`character_accountwide_taxi`;
-CREATE TABLE `characters`.`character_accountwide_taxi` (
+DROP TABLE IF EXiSTS `character_accountwide_taxi`;
+CREATE TABLE `character_accountwide_taxi` (
   `accountId` INT UNSIGNED NOT NULL,
   `faction` INT UNSIGNED NOT NULL,
   `node` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`accountId`, `faction`, `node`));
 
-DROP TABLE IF EXiSTS `characters`.`character_accountwide_title`;
-CREATE TABLE `characters`.`character_accountwide_title` (
+DROP TABLE IF EXiSTS `character_accountwide_title`;
+CREATE TABLE `character_accountwide_title` (
   `accountId` INT UNSIGNED NOT NULL,
   `title` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`accountId`, `title`));
 
-DROP TABLE IF EXiSTS `characters`.`character_accountwide_mount`;
-CREATE TABLE `characters`.`character_accountwide_mount` (
+DROP TABLE IF EXiSTS `character_accountwide_mount`;
+CREATE TABLE `character_accountwide_mount` (
   `accountId` INT UNSIGNED NOT NULL,
   `spellId` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`accountId`, `spellId`));
