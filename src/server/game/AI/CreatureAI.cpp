9771efe7709c2c60e1d1779d75f51e3db8b2b439
Jelle Meeus <sogladev@gmail.com>
2024-10-15 20:49:22 +0200

    WIP

--- src/server/game/AI/CreatureAI.cpp
+++ src/server/game/AI/CreatureAI.cpp
@@ -30,6 +30,7 @@
 #include "MapReference.h"
 #include "MotionMaster.h"
 #include "ObjectAccessor.h"
+#include "GuardMgr.h"
 #include "Player.h"
 #include "SpellMgr.h"
 #include "SpellHistory.h"
@@ -132,6 +133,21 @@ void CreatureAI::MoveInLineOfSight(Unit* who)
 
     if (me->HasReactState(REACT_AGGRESSIVE) && me->CanStartAttack(who, false))
         me->EngageWithTarget(who);
+
+    if (me->CanSummonGuards() && who->IsPlayer())
+    {
+        if (!me->CanFly() && me->GetDistanceZ(who) > CREATURE_Z_ATTACK_RANGE)
+            return;
+        if (who->isTargetableForAttack() && me->IsHostileTo(who) && who->isInAccessiblePlaceFor(me))
+        {
+            if (me->IsWithinDistInMap(who, me->GetAttackDistance(who), true, false) && me->IsWithinLOSInMap(who))
+            {
+                if (me->GetEntry() == 66)
+                    TC_LOG_ERROR("sql.sql", "MoveInLoS!");
+                sGuardMgr->SummonGuard(me, who);
+            }
+        }
+    }
 }
 
 void CreatureAI::OnOwnerCombatInteraction(Unit* target)
@@ -288,6 +304,21 @@ void CreatureAI::EngagementStart(Unit* who)
     _isEngaged = true;
 
     me->AtEngage(who);
+}
+
+void CreatureAI::JustStartedThreateningMe(Unit* who)
+{
+    if (me->CanSummonGuards())
+    {
+        if (me->GetEntry() == 66)
+            TC_LOG_ERROR("sql.sql", "EngagementStart!");
+        sGuardMgr->SummonGuard(me, who, true);
+    }
+
+    if (!IsEngaged())
+    {
+        EngagementStart(who);
+    }
 }
 
 void CreatureAI::EngagementOver()
@@ -513,7 +544,7 @@ void CreatureAI::Backpedal()
     float MaxRange = me->GetCollisionRadius() + target->GetCollisionRadius();
     if (! me->IsInDist(target, MaxRange))
         return;
-    
+
     me->SetFacingToObject(target);
     me->GetMotionMaster()->MoveBackpedal(target, me->GetMeleeRange(target) / 1.5);
 }
